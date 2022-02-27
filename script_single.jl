61dba856cfc2eb3f540c31215f3970a43f27dd13
iwasnothing <iwasnothing@gmail.com>
2022-02-27 20:29:59 +0800

    rename

---
+++ script_single.jl
@@ -0,0 +1,17 @@
+
+using GoldMinerTradeSignal
+
+parmfile = "https://iwasnothing.github.io/GoldMinerTradeSignal/GDX_parm.csv"
+
+
+m = formatMsg(getSignal(parmfile))
+
+
+open("results.html", "w") do io
+    write(io, m)
+end
+
+ENV["RESULTS"] = m
+ENV["RESULTS_FILE"] = "results.html"
+
+
