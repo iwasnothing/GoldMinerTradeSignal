1a284f7126e41de55584645b0492e8f10f2e6d0c
iwasnothing <iwasnothing@gmail.com>
2022-03-05 14:23:21 +0800

    new result

--- bin/main.jl
+++ bin/main.jl
@@ -1,20 +1,35 @@
 
 push!(LOAD_PATH, abspath(normpath(joinpath(@__DIR__, "../src/"))))
 using GoldMinerTradeSignal
+using Dates
 
-parmfile = "https://iwasnothing.github.io/GoldMinerTradeSignal/GDX_parm.csv"
-@show parmfile
-m1 = formatMsg(getSignal(parmfile))
-parmfile = "https://iwasnothing.github.io/GoldMinerTradeSignal/NVDA_parm.csv"
-@show parmfile
-m2 = formatMsg(getSignal(parmfile))
-msg = m1*m2
-@show msg
-open("results.html", "w") do io
-    write(io, msg)
+function runDaily()
+    parmfile = "https://iwasnothing.github.io/GoldMinerTradeSignal/GDX_parm.csv"
+    @show parmfile
+    m1 = formatMsg(getSignal(parmfile))
+    parmfile = "https://iwasnothing.github.io/GoldMinerTradeSignal/NVDA_parm.csv"
+    @show parmfile
+    m2 = formatMsg(getSignal(parmfile))
+    msg = m1*m2
+    @show msg
+    open("results.html", "w") do io
+        write(io, msg)
+    end
+
+    commitcmd = `git commit -am "new result"`
+    run(commitcmd)
+    pushcmd = `git push origin master`
+    run(pushcmd)
 end
 
-commitcmd = `git commit -am "new result"`
-run(commitcmd)
-pushcmd = `git push origin master`
-run(pushcmd)
+function mainLoop()
+    while true
+        t = Dates.now()
+        if Hour(t) == Hour(14)
+            runDaily()
+        end
+        sleep(3600)
+    end
+end
+
+mainLoop()
